ed589064fa8866bfa5a0b64d30bdce0fa257acd3
beboo22 <mt557829@gmail.com>
2023-11-07 23:13:22 +0200

    task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,27 +3,36 @@
 2-main
 """
 import requests
+recurse = __import__('2-recurse').recurse
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, word_count=None):
     """
     count words
     """
+    if word_count is None:
+        word_count = {word: 0 for word in word_list}
+
     subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers={"User-Agent": "My-User-Agent"}
-    sub_info = requests.get(subreddit_url,headers)
+    headers = {"User-Agent": "My-User-Agent"}
+    sub_info = requests.get(subreddit_url, headers=headers)
+
     if sub_info.status_code != 200:
         return None
+
     lis = recurse(subreddit)
+
     if not lis:
         return None
+
     for title in lis:
         for word in word_list:
             for s in title:
-                if s == word:
+                if s.lower() == word.lower():
                     word_count[word] += 1
+
     if sub_info.json().get("data").get("after"):
         return count_words(subreddit, word_list, word_count)
     else:
-        for k, v in word_count:
+        for k, v in word_count.items():
             print(k, ": ", v)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,12 +10,12 @@ def recurse(subreddit, hot_list=[], s=0):
     subscribers
     """
     subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers={"User-Agent": "My-User-Agent"}
-    sub_info = requests.get(subreddit_url,headers)
+    headers = {"User-Agent": "My-User-Agent"}
+    sub_info = requests.get(subreddit_url, headers)
     if sub_info.status_code < 300:
         if len(sub_info.json().get("data").get("children")) > s:
             hot_list.append(sub_info.json().get("data").get("children")[s]
-                               .get("data").get("title"))
+                            .get("data").get("title"))
         if sub_info.json().get("data").get("after"):
             return recurse(subreddit, hot_list, s + 1)
         else:
