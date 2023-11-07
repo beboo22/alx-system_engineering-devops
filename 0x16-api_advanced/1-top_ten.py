2df4ccf17e59b944a87cd563b1de3b3750e60647
beboo22 <mt557829@gmail.com>
2023-11-07 21:54:08 +0200

    task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
 
-    subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)"
@@ -18,10 +18,11 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     geting_inf = requests.get(subreddit_url, headers=headers, params=params)
 
-    if response.status_code == 404:
+    if geting_inf.status_code == 404:
         print("None")
         return
 
     data = geting_inf.json().get("data")
+
     for c in data.get("children"):
         print(c.get('data').get("title"))
